d00ef619f61673edbeba529db0d8b48451cd94ab
Johnny Goodman <johnny.goodman@gmail.com>
2012-05-27 23:13:19 -0500

    Random selection of an about us item. Making the about page the / path

--- theweb.rb
+++ theweb.rb
@@ -4,9 +4,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 enable :sessions
 
-get '/' do
-	erb :dashboard
-end
+# get '/' do
+#   erb :dashboard
+# end
 
 post '/number' do
 	@number_of_randoms = session[:number_of_randoms] || 0
@@ -17,6 +17,12 @@ post '/number' do
 	erb :number
 end
 
-get '/about' do 
+get '/' do 
+  about_me = [{url: 'http://www.cpap.com/aboutUsPerson.php?AboutUsID=6', anchor: 'CPAP.com About Us Page'}, 
+               {url: 'http://www.linkedin.com/profile/view?id=6892236', anchor: 'Linked In Profile'},
+               {url: 'https://github.com/johnnygoodman', anchor: 'Github Profile'}]                 
+  selected = about_me.shuffle.take(1).first
+  @about_me_url = selected.fetch(:url)
+  @about_me_anchor = selected.fetch(:anchor)   
   erb :about
 end
