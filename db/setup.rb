83b5322de2eb62e6f78199ab6560596a72a93b5b
meltar <meltar@gmail.com>
2013-04-24 01:45:48 -0400

    More clean up to remove changes for production

--- db/setup.rb
+++ db/setup.rb
@@ -1,32 +1,17 @@
 require 'pg'
 require 'active_record'
 
-configure(:development) do
-	require 'yaml'
+require 'yaml'
 
-	connection_details = YAML::load(File.open('config/database.yml'))
+connection_details = YAML::load(File.open('config/database.yml'))
 
-	# Setup out connection details
-	ActiveRecord::Base.establish_connection(connection_details.merge(
-		{'database'=> 'postgres', 'schema_search_path'=> 'public'}))
-	# create the 'tv' database 
-	ActiveRecord::Base.connection.drop_database (connection_details.fetch('database')) rescue nil
-	ActiveRecord::Base.connection.create_database(connection_details.fetch('database')) rescue nil
-	# connect to it
-	ActiveRecord::Base.establish_connection(connection_details)
-	# Migrate all the things
-	ActiveRecord::Migrator.migrate("db/migrate/")
-end
-#configure :production, :development do
-configure :production do
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
- 
-	ActiveRecord::Base.establish_connection(
-			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-			:host     => db.host,
-			:username => db.user,
-			:password => db.password,
-			:database => db.path[1..-1],
-			:encoding => 'utf8'
-	)
-end
+# Setup out connection details
+ActiveRecord::Base.establish_connection(connection_details.merge(
+	{'database'=> 'postgres', 'schema_search_path'=> 'public'}))
+# create the 'tv' database 
+ActiveRecord::Base.connection.drop_database (connection_details.fetch('database')) rescue nil
+ActiveRecord::Base.connection.create_database(connection_details.fetch('database')) rescue nil
+# connect to it
+ActiveRecord::Base.establish_connection(connection_details)
+# Migrate all the things
+ActiveRecord::Migrator.migrate("db/migrate/")
