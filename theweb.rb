64709bf24659335b953328b5879d9a4c4c9f74eb
PDaily <pat.irwin4@gmail.com>
2014-04-26 21:57:47 +0000

    Added some more things I should like

--- theweb.rb
+++ theweb.rb
@@ -6,7 +6,7 @@ enable :sessions
 
 get '/' do
   @title = "Patrick Irwin"
-  likes = ["Turtles", "Cake", "Puppies", "Ruby"]
+  likes = ["Turtles", "Cake", "Puppies", "Ruby", "Archer (The Cartoon)", "PC Games", "Stuff", "Apples", "Programming"]
   @random_likes = likes[rand(likes.length)]
 	erb :dashboard
 end
